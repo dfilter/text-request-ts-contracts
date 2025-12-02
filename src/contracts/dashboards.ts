d162661f29525f6c01a15c4b8e0e9758ba9bcb7c
dirk <dirk@maplelane.ag>
2025-12-02 09:10:55 -0500

    add summaries and descriptions

--- src/contracts/dashboards.ts
+++ src/contracts/dashboards.ts
@@ -1,16 +1,15 @@
 import { initContract } from "@ts-rest/core";
 import {
+  dashboardIdPathParamsSchema,
+  errorResponseSchema,
+  paginationQuerySchema,
+} from "../lib/zod/common.schemas";
+import {
   dashboardCollectionSchema,
   dashboardSchema,
   provisioningQuerySchema,
   provisioningResponseSchema,
 } from "../lib/zod/dashboard.schemas";
-import {
-  dashboardIdPathParamsSchema,
-  errorResponseSchema,
-  paginationQuerySchema,
-  unknownResponseSchema,
-} from "../lib/zod/common.schemas";
 
 const contract = initContract();
 
@@ -24,6 +23,9 @@ const dashboardContract = contract.router({
       400: errorResponseSchema,
       401: errorResponseSchema,
     },
+    summary: "Create a new dashboard with the given name and phone number",
+    description:
+      "Creates a new dashboard in the account. Once the dashboard is created, admins will be emailed about the changed, and can begin assigning users. This endpoint can make a dashboard for any provisioned numbers. You can search for provisioned numbers in a specific area code using GET /dashboards/provisioning. If the phone number is already in use, a Bad Request exception will be thrown.",
   },
   collection: {
     method: "GET",
@@ -33,6 +35,8 @@ const dashboardContract = contract.router({
       200: dashboardCollectionSchema,
       401: errorResponseSchema,
     },
+    summary: "Get all dashboards in an account.",
+    description: "Returns all dashboards in an account.",
   },
   get: {
     method: "GET",
@@ -43,6 +47,8 @@ const dashboardContract = contract.router({
       400: errorResponseSchema,
       401: errorResponseSchema,
     },
+    summary: "Get info on this specific dashboard",
+    description: "Returns the name and phone of the dashboard.",
   },
   provisioning: {
     method: "GET",
@@ -53,6 +59,9 @@ const dashboardContract = contract.router({
       400: errorResponseSchema,
       401: errorResponseSchema,
     },
+    summary: "Get available provisioned numbers for hosting with a dashboard",
+    description:
+      "Returns a list of 30 provisioning numbers. These numbers can be used for creating a dashboard with POST /dashboards. All numbers and area codes must be in the United States. Text Request only hosts numbers with the +1 country code.",
   },
   update: {
     method: "PUT",
@@ -63,17 +72,21 @@ const dashboardContract = contract.router({
       200: dashboardSchema,
       401: errorResponseSchema,
     },
+    summary: "Update a specific dashboard's name",
+    description:
+      "Updates a dashboard. This does not change a dashboard's phone number. If you want a new number with another dashboard's contacts, you will have to create a new dashboard with a POST and add the contacts with a bulk export/import.",
   },
   delete: {
     method: "DELETE",
     path: "/dashboards/:dashboard_id",
     pathParams: dashboardIdPathParamsSchema,
     responses: {
-      204: unknownResponseSchema,
-      400: unknownResponseSchema,
+      204: contract.noBody(),
+      400: contract.noBody(),
       401: errorResponseSchema,
-      404: unknownResponseSchema,
+      404: contract.noBody(),
     },
+    summary: "Deletes the specified dashboard.",
   },
 });
 
