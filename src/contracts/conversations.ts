0a9586290de1c7e96c737cb24d89e5ec64707995
dirk <dirk@maplelane.ag>
2025-12-02 08:15:37 -0500

    add summaries and descriptions

--- src/contracts/conversations.ts
+++ src/contracts/conversations.ts
@@ -25,6 +25,9 @@ const conversationContract = contract.router({
       204: unknownResponseSchema,
       401: errorResponseSchema,
     },
+    summary: "Retrieves all conversations for this dashboard.",
+    description:
+      "Retrieves the last message across all conversations in this dashboard.",
   },
   contactCollection: commonContract.contactMessageCollection,
   dashboardMessageCollection: commonContract.dashboardMessageCollection,
@@ -37,6 +40,9 @@ const conversationContract = contract.router({
       200: unknownResponseSchema,
       401: errorResponseSchema,
     },
+    summary: "Archive a Conversation",
+    description:
+      "Archiving a conversation removes it from the results of all conversation retrieval endpoints. A conversation will be automatically unarchive if the customer sends a text to your Text Request number, or if you send the customer a text from your Text Request number. Archiving conversations is a good way to remove clutter from customers that you don’t plan on talking to in the future. Note that if a customer texts in 'STOP' to prevent you from sending them any further SMS messages, the conversation is automatically archived.",
   },
   unarchive: {
     method: "PUT",
@@ -47,6 +53,9 @@ const conversationContract = contract.router({
       200: unknownResponseSchema,
       401: errorResponseSchema,
     },
+    summary: "Unarchive a Conversation",
+    description:
+      "Unarchive a conversation. Once a conversation is unarchived, it will once again appear in the queue. it will also appear in conversations returned by the /conversations endpoint. If a conversation is already unarchived, this method has no effect.",
   },
 });
 
